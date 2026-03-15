fcd8f298d0aae671a6062c0f5bb6d6cdfac81993
displaying username <radoslavbalog529@gmail.com>
2026-03-15 04:50:05 +0000

    Add licensing comments to tinyset.js
    Added licensing information for GNU General Public License.

--- tinyset.js
+++ tinyset.js
@@ -1,3 +1,16 @@
+//    Copyright (C) 2026  R Balog
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, version 3 of the License
+//
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // tinyset.js v2.6
 export const where={
 eq:(k,v)=>i=>i[k]===v,ne:(k,v)=>i=>i[k]!==v,gt:(k,v)=>i=>i[k]>v,gte:(k,v)=>i=>i[k]>=v,
